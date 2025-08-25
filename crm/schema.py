150cdaf7b325484e34b30f24bfef58aef40b7f6a
Frankie-lii <oyiengofranklne49@gmail.com>
2025-08-25 02:16:26 -0700

    task 0

--- crm/schema.py
+++ crm/schema.py
@@ -1,5 +1,5 @@
 import graphene
-from .models import Product
+from crm.models import Product  # REQUIRED for Product model
 
 class ProductType(graphene.ObjectType):
     id = graphene.ID()
@@ -7,20 +7,19 @@ class ProductType(graphene.ObjectType):
     stock = graphene.Int()
 
 class UpdateLowStockProducts(graphene.Mutation):
-    class Output:
-        updated_products = graphene.List(ProductType)
-        message = graphene.String()
+    updated_products = graphene.List(ProductType)
+    message = graphene.String()
 
     def mutate(self, info):
         low_stock_products = Product.objects.filter(stock__lt=10)
         updated = []
 
         for product in low_stock_products:
-            product.stock += 10  # simulate restocking
+            product.stock += 10
             product.save()
             updated.append(product)
 
-        return UpdateLowStockProducts.Output(
+        return UpdateLowStockProducts(
             updated_products=updated,
             message=f"{len(updated)} products updated successfully."
         )
